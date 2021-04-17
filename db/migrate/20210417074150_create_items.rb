813969f81b9f7c1ce290bb163a2fb86a8873147f
priscillahuynh <priscillamhuynh@gmail.com>
2021-04-17 11:52:46 +0400

    indexed list_id on items table

--- db/migrate/20210417074150_create_items.rb
+++ db/migrate/20210417074150_create_items.rb
@@ -2,9 +2,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.string :description
-      t.integer :list_id
-
-      t.timestamps
+      t.belongs_to :list, foreign_key: true
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_073511) do
+ActiveRecord::Schema.define(version: 2021_04_17_074150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string "description"
-    t.integer "list_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "list_id"
+    t.index ["list_id"], name: "index_items_on_list_id"
   end
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "items", "lists"
 end
