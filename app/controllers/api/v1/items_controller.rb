bc8413e9d3527c2e5324d39005eea2a325096a6b
priscillahuynh <priscillamhuynh@gmail.com>
2021-04-17 12:45:48 +0400

    update items controller with index and create actions

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items
+    end 
+
+    def create
+        item = Item.new(item_params)
+        if item.save
+            render json: item, status: :accepted
+        else 
+            render json: {errors: item.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def item_params
+        params.require(:item).permit(:description, :list_id)
+    end
 end
