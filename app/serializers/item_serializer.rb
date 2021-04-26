3970e540ff8a65d07e333dccfb010a81a29f901e
priscillahuynh <priscillamhuynh@gmail.com>
2021-04-26 20:19:28 +0400

    modify serializers to test different endpoints

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
   attributes :description, :list_id, :list
-  belongs_to :list
+  # belongs_to :list, serializer: ListSerializer
 end
--- app/serializers/list_serializer.rb
+++ app/serializers/list_serializer.rb
@@ -1,4 +1,5 @@
 class ListSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :items
+  has_many :items, serializer: ItemSerializer
 end
