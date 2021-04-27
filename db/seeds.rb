6bfb90678a23638e965b7dd6f1ae2f55dd2411dc
priscillahuynh <priscillamhuynh@gmail.com>
2021-04-27 13:11:51 +0400

    reset database info for project walkthrough

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@ Item.destroy_all
 List.destroy_all
 
 groceries = List.create(title: "Groceries")
-to_do = List.create(title: "To Do")
-to_read = List.create(title: "To Read")
+# to_do = List.create(title: "To Do")
+# to_read = List.create(title: "To Read")
 
 Item.create(description: "Bread", list_id: groceries.id)
 Item.create(description: "Cheese", list_id: groceries.id)
 Item.create(description: "Apples", list_id: groceries.id)
 
-Item.create(description: "Pick up dry cleaning", list_id: to_do.id)
-Item.create(description: "Wash car", list_id: to_do.id)
-Item.create(description: "Make dinner", list_id: to_do.id)
+# Item.create(description: "Pick up dry cleaning", list_id: to_do.id)
+# Item.create(description: "Wash car", list_id: to_do.id)
+# Item.create(description: "Make dinner", list_id: to_do.id)
 
-Item.create(description: "Atomic Habits", list_id: to_read.id)
-Item.create(description: "The Monk Who Sold His Ferrari", list_id: to_read.id)
-Item.create(description: "Javascript & Jquery", list_id: to_read.id)
+# Item.create(description: "Atomic Habits", list_id: to_read.id)
+# Item.create(description: "Sapiens", list_id: to_read.id)
+# Item.create(description: "Javascript & Jquery", list_id: to_read.id)
